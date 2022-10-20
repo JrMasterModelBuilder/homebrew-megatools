d0575db3a2a6feb689616de4eb35814e95114969
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-10-20 02:39:36 -0400

    Lint fix

--- Formula/megatools-stable.rb
+++ Formula/megatools-stable.rb
@@ -11,9 +11,9 @@ class MegatoolsStable < Formula
   end
 
   depends_on "asciidoc" => :build
-  depends_on "meson" => :build
   depends_on "cmake" => :build
   depends_on "docbook2x" => :build
+  depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
