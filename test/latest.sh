9eff72bd655e66437a6a52a90738e3821b4167de
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-06-02 00:36:07 -0400

    Check latest with script

---
+++ test/latest.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+url='https://megatools.megous.com/builds/LATEST'
+expected='megatools-1.11.1.20230212'
+
+version="$(curl -f -L -s "${url}")"
+if [[ "${version}" == "${expected}" ]]; then
+	echo "Verified version"
+else
+	echo "Unexpect version: ${version}"
+	exit 1
+fi
