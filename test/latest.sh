8eccfa3370edae10f86eb9bd9682fa6fd4933865
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2026-01-30 23:58:18 -0500

    Try harder to check if latest

--- test/latest.sh
+++ test/latest.sh
@@ -5,11 +5,89 @@ set -o pipefail
 
 url='https://xff.cz/megatools/builds/LATEST'
 expected='megatools-1.11.5.20250706'
+exprefix='megatools-'
 
-version="$(curl -k -f -L -s "${url}")"
-if [[ "${version}" == "${expected}" ]]; then
-	echo "Verified version"
+torhost='127.0.0.1'
+torport='9050'
+torctrl='9051'
+torprox="socks5://${torhost}:${torport}"
+torpass=''
+
+torcmd() {
+	printf 'AUTHENTICATE "%s"\r\n%s\r\n' "${torpass}" "$1" |\
+		nc "${torhost}" "${torctrl}" 2>/dev/null
+}
+
+torprogress() {
+	local progress='0'
+	local bootstrap=$(torcmd 'GETINFO status/bootstrap-phase')
+	if [[ "${bootstrap}" == *'PROGRESS='* ]]; then
+		local part="${bootstrap#*PROGRESS=}"
+		progress="${part%% *}"
+	fi
+	printf '%s' "${progress}"
+}
+
+torwait() {
+	local prev=''
+	local curr='0'
+	while true; do
+		curr="$(torprogress)"
+		if [[ "${curr}" != "${prev}" ]]; then
+			echo "Tor progress: ${curr}%"
+			prev="${curr}"
+		fi
+		if [[ "${curr}" == '100' ]]; then
+			break
+		fi
+		sleep 0.1
+	done
+}
+
+tornew() {
+	torcmd 'SIGNAL NEWNYM'
+	torwait
+}
+
+cleanup() {
+	if [[ "${torpass}" != '' ]]; then
+		echo 'Tor shutdown...'
+		torcmd 'SIGNAL SHUTDOWN'
+		torpass=''
+	fi
+}
+
+trap cleanup EXIT INT TERM
+
+response=''
+for i in {1..10}; do
+	echo "Attempt: ${i}"
+	if [[ "${i}" == 2 ]]; then
+		torpass="$(uuidgen)"
+		tor \
+			--RunAsDaemon 1 \
+			--SocksPort "${torport}" \
+			--ControlPort "${torctrl}" \
+			--HashedControlPassword "$(tor --hash-password "${torpass}")"
+		torwait
+	fi
+	if [[ "${i}" != 1 ]]; then
+		if [[ "${i}" != 2 ]]; then
+			tornew
+		fi
+		response="$(curl -v --max-time 5 -k -f -L -s --proxy "${torprox}" "${url}" || true)"
+	else
+		response="$(curl -v --max-time 5 -k -f -L -s "${url}" || true)"
+	fi
+	if [[ "${response}" == "${exprefix}"* ]]; then
+		break
+	fi
+done
+echo "RESPONSE: ${response}"
+
+if [[ "${response}" == "${expected}" ]]; then
+	echo 'PASS: Verified version'
 else
-	echo "Unexpect version: ${version}"
+	echo 'FAIL: Unexpect version'
 	exit 1
 fi
