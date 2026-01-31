f657c497f4eaef33bcb3e7305f76b7ec9887f471
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2026-01-30 22:31:48 -0500

    Debug connection

--- test/latest.sh
+++ test/latest.sh
@@ -6,7 +6,7 @@ set -o pipefail
 url='https://xff.cz/megatools/builds/LATEST'
 expected='megatools-1.11.5.20250706'
 
-version="$(curl -k -f -L -s "${url}")"
+version="$(curl -v -k -f -L -s "${url}")"
 if [[ "${version}" == "${expected}" ]]; then
 	echo "Verified version"
 else
