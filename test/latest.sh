0de87286673cf279b536b8913edc10b02d05f13d
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2026-01-30 22:40:08 -0500

    Use proxy to access latest file

--- test/latest.sh
+++ test/latest.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-url='https://xff.cz/megatools/builds/LATEST'
+url='https://api.allorigins.win/raw?url=https://xff.cz/megatools/builds/LATEST'
 expected='megatools-1.11.5.20250706'
 
 version="$(curl -v -k -f -L -s "${url}")"
